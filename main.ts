47cc7205bdd90b8b9891fa53a99cee40c8de8dcf
uni2008 <93262463+uni2008@users.noreply.github.com>
2022-01-12 14:09:59 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (1 == receivedNumber) {
+        basic.showString("A")
+    }
+    if (2 == receivedNumber) {
+        basic.showString("B")
+    }
+    if (3 == receivedNumber) {
+        basic.showString("A+B")
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(6)
